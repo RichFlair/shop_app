53c3b829d49f8a7e03aa9160d943a90b0b5824b6
Andrew Marfo <andrewmarfo23@gmail.com>
2023-11-16 14:04:27 +0000

    auth screen

--- lib/screens/authScreen.dart
+++ lib/screens/authScreen.dart
@@ -19,16 +19,114 @@ class AuthScreen extends StatelessWidget {
               decoration: BoxDecoration(
                 gradient: LinearGradient(
                   colors: [
-                    const Color.fromARGB(255, 97, 38, 164).withOpacity(1),
-                    const Color.fromARGB(255, 167, 118, 35).withOpacity(1)
+                    const Color.fromARGB(255, 97, 38, 164).withOpacity(0.9),
+                    const Color.fromARGB(255, 167, 118, 35).withOpacity(0.9)
                   ],
                   begin: Alignment.topRight,
                   end: Alignment.bottomLeft,
                 ),
               ),
             ),
+          ),
+          SingleChildScrollView(
+            child: SizedBox(
+              height: screenSize.height,
+              width: screenSize.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: Container(
+                      margin: const EdgeInsets.only(bottom: 24),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 90,
+                      ),
+                      transform: Matrix4.rotationZ(270),
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 130, 48, 19),
+                        borderRadius: BorderRadius.circular(25),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black54,
+                            blurRadius: 8,
+                            offset: Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: const Text(
+                        'MyShop',
+                        style: TextStyle(
+                            fontSize: 50,
+                            color: Colors.white,
+                            fontFamily: 'Anon',
+                            fontWeight: FontWeight.normal),
+                      ),
+                    ),
+                  ),
+                  AuthCard()
+                ],
+              ),
+            ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class AuthCard extends StatefulWidget {
+  const AuthCard({super.key});
+
+  @override
+  State<AuthCard> createState() => _AuthCardState();
+}
+
+class _AuthCardState extends State<AuthCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 8,
+      margin: const EdgeInsets.symmetric(horizontal: 40),
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: 'E-Mail'),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                    ),
+                    obscureText: true,
+                    autocorrect: false,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('LOGIN'),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Not registered? SIGNUP'),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
       ),
     );
   }
