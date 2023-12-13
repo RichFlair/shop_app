b7846a058378ea9cb7121db07bf5bf5cd8c5bb26
Andrew Marfo <andrewmarfo23@gmail.com>
2023-12-13 18:28:04 +0000

    adding a logout funtion

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../providers/auth.dart';
 import '../screens/orders_screen.dart';
 import '../screens/user_products_screen.dart';
 
@@ -57,6 +59,16 @@ class DrawerWidget extends StatelessWidget {
             },
           ),
           const Divider(),
+          const Spacer(),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('Logout'),
+            trailing: const Icon(Icons.forward),
+            onTap: () {
+              Navigator.of(context).pop();
+              Provider.of<Auth>(context, listen: false).logout();
+            },
+          ),
         ],
       ),
     );
