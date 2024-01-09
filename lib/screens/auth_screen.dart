b38919ceb6b5f8f6c974e44f33ea25470897fba9
Andrew Marfo <andrewmarfo23@gmail.com>
2024-01-09 23:49:55 +0000

    modififying auth screen

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -110,9 +110,14 @@ class _AuthCardState extends State<AuthCard>
       ),
     );
     _heightAnimation = Tween<Size>(
-            begin: Size(double.infinity, 260), end: Size(double.infinity, 320))
-        .animate(
-            CurvedAnimation(parent: _controller!, curve: Curves.fastOutSlowIn));
+      begin: Size(double.infinity, 350),
+      end: Size(double.infinity, 280),
+    ).animate(
+      CurvedAnimation(
+        parent: _controller!,
+        curve: Curves.fastOutSlowIn,
+      ),
+    );
   }
 
   Map<String, String> credentials = {
