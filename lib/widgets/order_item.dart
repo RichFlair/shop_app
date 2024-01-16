8b49bcd61b89d03f26bdb1583c8d024ce037110b
Andrew Marfo <andrewmarfo23@gmail.com>
2024-01-16 22:54:13 +0000

    adding animation: animating the order box

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -22,61 +22,65 @@ class _OrderItemState extends State<OrderItem> {
   var _expanded = false;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
-            subtitle: Text(
-              DateFormat('dd MM yyyy hh:mm').format(widget.order.dateTime),
-            ),
-            trailing: IconButton(
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-              icon: _expanded
-                  ? const Icon(Icons.expand_less)
-                  : const Icon(Icons.expand_more),
-            ),
-          ),
-          if (_expanded)
-            AnimatedContainer(
-              duration: const Duration(milliseconds: 400),
-              // height: _expanded ? widget.noOfOrderItems * 10 : 0,
-              constraints: BoxConstraints(
-                  minHeight: _expanded ? widget.noOfOrderItems * 10 : 0),
-              child: SizedBox(
-                height: min(widget.order.products.length * 10.0 + 100, 100),
-                child: ListView(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 10),
-                    children: widget.order.products.map((e) {
-                      return Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            e.title,
-                            style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            '${e.quantiy}  x  ${e.price}',
-                            style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      );
-                    }).toList()),
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      height: _expanded ? min(widget.noOfOrderItems * 20 + 200, 200) : 95,
+      child: Card(
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
+              subtitle: Text(
+                DateFormat('dd MM yyyy hh:mm').format(widget.order.dateTime),
+              ),
+              trailing: IconButton(
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+                icon: _expanded
+                    ? const Icon(Icons.expand_less)
+                    : const Icon(Icons.expand_more),
               ),
             ),
-        ],
+            if (_expanded)
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 400),
+                // height: _expanded ? widget.noOfOrderItems * 10 : 0,
+                constraints: BoxConstraints(
+                    minHeight: _expanded ? widget.noOfOrderItems * 10 : 0),
+                child: SizedBox(
+                  height: min(widget.order.products.length * 10.0 + 100, 100),
+                  child: ListView(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      children: widget.order.products.map((e) {
+                        return Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              e.title,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              '${e.quantiy}  x  ${e.price}',
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        );
+                      }).toList()),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
