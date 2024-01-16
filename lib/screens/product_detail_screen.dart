ef8f4ffb630e2962c0bde27c29f50d3b1da262f7
Andrew Marfo <andrewmarfo23@gmail.com>
2024-01-16 19:21:37 +0000

    adding animation: custom scroll view

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -14,40 +14,52 @@ class ProductDetailScreen extends StatelessWidget {
       listen: false,
     ).getProductId(productId);
     return Scaffold(
-        appBar: AppBar(
-          title: Text(productData.title),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 300,
-                width: double.infinity,
-                child: Hero(
-                  tag: productData.id,
-                  child: Image.network(
-                    productData.imageUrl,
-                    fit: BoxFit.fitWidth,
+      // appBar: AppBar(
+      //   title: Text(productData.title),
+      // ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(productData.title),
+              centerTitle: true,
+              background: Hero(
+                tag: productData.id,
+                child: Image.network(
+                  productData.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            pinned: true,
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                const SizedBox(height: 10),
+                Text(
+                  '\$${productData.price}',
+                  style: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 22,
                   ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                '\$${productData.price}',
-                style: const TextStyle(
-                  color: Colors.grey,
-                  fontSize: 22,
+                const SizedBox(height: 10),
+                Text(
+                  productData.description,
+                  style: const TextStyle(
+                    fontSize: 16,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                productData.description,
-                style: const TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-            ],
+                const SizedBox(
+                  height: 1000,
+                )
+              ],
+            ),
           ),
-        ));
+        ],
+      ),
+    );
   }
 }
